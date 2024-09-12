07406846e2cb9f2d635c96e8071c6983d3bc32a9
secretindex <caio.smelo04@gmail.com>
2024-09-12 16:47:49 -0300

    gmail config rollback

--- src/mailer.js
+++ src/mailer.js
@@ -4,7 +4,11 @@ const transport = nodemailer.createTransport({
   service: "gmail",
   auth: {
     type: "OAuth2",
-    user: "Buxtehude",
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.OAUTH_CLIENTID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
   },
 });
 
